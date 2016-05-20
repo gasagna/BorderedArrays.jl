6f032475a2908f2ea6796f3ad2c5ae2bb5948e6a
Davide Lasagna <davide.lasagna@soton.ac.uk>
2016-05-20 19:55:03 +0100

    Removed BlockDiagonal stuff

--- src/BorderedArrays.jl
+++ src/BorderedArrays.jl
@@ -19,94 +19,7 @@ import Base.LinAlg: A_ldiv_B!,
                     Ac_ldiv_B!
 
 export BorderedMatrix,
-       BorderedVector,
-       BlockDiagonalMatrix,
-       BlockVector,
-       nblocks,
-       blocksize
-
-# ~~~ Block Vector ~~~
-
-# type to store a vector-of-vectors
-type BlockVector{T<:Number} <: AbstractVector{T}
-    data::Matrix{T} # the i-th column is i-th vector
-end
-
-# block properties
-function _blockstart(v::BlockVector, i::Integer) 
-    0 < i <= nblocks(v) || throw(BoundsError("wrong input index"))
-    (i-1)*blocksize(v) + 1
-end
-nblocks(v::BlockVector) = size(v.data, 2)
-blocksize(v::BlockVector) = size(v.data, 1)
-
-# array interface
-size(v::BlockVector) = (length(v), )
-length(v::BlockVector) = length(v.data)
-eltype{T}(v::BlockVector{T}) = T
-linearindexing(v::BlockVector) = LinearSlow()
-getindex(v::BlockVector, i::Integer) = v.data[i]
-setindex!(v::BlockVector, val, i::Integer) = (v.data[i] = val)
-
-# dot product
-function dot{T}(v::BlockVector{T}, u::BlockVector{T})
-    length(v) == length(u) || 
-        error(DimensionMismatch("block vectors must have same length"))
-    vdata, udata = v.data, u.data
-    out = zero(T)
-    @simd for i = 1:length(v)
-        @inbounds out += vdata[i]*udata[i]
-    end
-    out
-end
-
-
-# ~~~ Block Diagonal Matrix ~~~
-
-# type to store a block diagonal matrix with square blocks
-type BlockDiagonalMatrix{T<:Number} <: AbstractMatrix{T}
-    data::Matrix{T} # column i is the i-th block stored in a column-major format
-    function BlockDiagonalMatrix(data::Matrix{T})
-        # can rearrange data in a diagonal block matrix with square blocks?
-        n = sqrt(size(data, 1))
-        ceil(n) - floor(n) == 0  || error("error in input data")
-        new(data)
-    end
-end
-BlockDiagonalMatrix{T}(data::Matrix{T}) = BlockDiagonalMatrix{T}(data)
-
-# pointer to first element of the ith block
-function _blockstart(v::BlockDiagonalMatrix, i::Integer) 
-    0 < i <= nblocks(v) || throw(BoundsError("wrong input index"))
-    (i-1)*blocksize(v)^2 + 1
-end
-
-# Number of blocks and block size
-nblocks(v::BlockDiagonalMatrix) = size(v.data, 2)
-blocksize(v::BlockDiagonalMatrix) = Int(sqrt(size(v.data, 1)))
-
-# array interface
-size(v::BlockDiagonalMatrix) = (g = nblocks(v)*blocksize(v); (g, g))
-eltype{T}(v::BlockDiagonalMatrix{T}) = T
-linearindexing(v::BlockDiagonalMatrix) = LinearSlow()
-
-# Global indexing 
-function getindex{T}(v::BlockDiagonalMatrix{T}, i::Integer, j::Integer)
-    n = blocksize(v)
-    # block and local coordinates
-    iglob, iloc = divrem(i+n-1, n)
-    jglob, jloc = divrem(j+n-1, n)
-    # if outside of diagonal return 0
-    val = iglob != jglob ? zero(T) : v.data[jloc*n + iloc+1, jglob]
-    return val
-end
-
-# Block indexing
-setindex!{T}(v::BlockDiagonalMatrix{T}, val, k, i::Integer, j::Integer) = 
-    v.data[i + (j-1)*blocksize(v),  Int(k)] = val
-getindex{T}(v::BlockDiagonalMatrix{T}, k, i::Integer, j::Integer) =
-    v.data[i + (j-1)*blocksize(v),  Int(k)]
-
+       BorderedVector
 
 # ~~~ Bordered Vector ~~~
 
@@ -162,9 +75,10 @@ BorderedMatrix{T}(M₁₁::AbstractMatrix{T},
     BorderedMatrix{T, typeof(M₁₁), typeof(M₁₂)}(M₁₁, M₁₂, M₂₁, M₂₂)
 
 # array interface
-eltype{T}(v::BlockDiagonalMatrix{T}) = T
+eltype{T}(v::BorderedMatrix{T}) = T
 size(M::BorderedMatrix) = (size(M._₁₁, 1) + 1, size(M._₁₁, 2) + 1)
 linearindexing(v::BorderedMatrix) = LinearSlow()
+
 function getindex(M::BorderedMatrix, i::Integer, j::Integer)
     m, n = size(M)
     if i < m
@@ -187,8 +101,8 @@ end
 # ~~~ Utilities ~~~
 
 # collect to a DenseArray - useful for debugging solvers
-full(v::Union{BlockVector, BorderedVector}) = collect(v)
-function full{T}(M::Union{BlockDiagonalMatrix{T}, BorderedMatrix{T}})
+full(v::BorderedVector) = collect(v)
+function full{T}(M::BorderedMatrix{T})
     m, n = size(M)
     Mdense = zeros(T, m, n)
     for i = 1:m, j = 1:n
@@ -198,97 +112,11 @@ function full{T}(M::Union{BlockDiagonalMatrix{T}, BorderedMatrix{T}})
 end
 
 
-# ~~~ Solvers ~~~
-
-const liblapack = Base.liblapack_name
-import Base.LinAlg: BlasInt
-import Base.LAPACK: getrs!, getrf!
-
-# ~~~ Linear algebra for block systems ~~~
-# copied form base
-immutable BlockLU{T} <: Factorization{T}
-    factors::BlockDiagonalMatrix{T}
-    ipiv::BlockVector{BlasInt}
-    info::Vector{BlasInt}
-end    
-
-# in-place LU factorisation with pivoting of a square block diagonal matrix 
-function lufact!{T}(A::BlockDiagonalMatrix{T})
-    A, ipiv, info = getrf!(A)
-    return BlockLU{T}(A, ipiv, info)
-end
-
-# A_ldiv_B business for block lu factorisation
-for typ in (Float64, Complex128)
-    for (ch, fname) in zip(('N', 'T'), (:A_ldiv_B!, :At_ldiv_B!))
-        @eval function $fname{typ}(F::BlockLU{typ}, B::BlockVector{typ})
-            # check there is no singular block
-            m, k = findmax(F.info)
-            m > 0 && throw(SingularException("block $k is singular"))
-            getrs!($ch, F.factors, F.ipiv, F.info, B) 
-        end
-    end
-end
-
-#  Custom LAPACK wrappers for block matrices 
-for (getrs, getrf, elty) in ((:zgetrs_, :zgetrf_, Complex128), 
-                             (:dgetrs_, :dgetrf_, Float64))
-    #DGETRF computes an LU factorization of a general M-by-N matrix A
-    # using partial pivoting with row interchanges.
-    #
-    # The factorization has the form
-    #    A = P * L * U
-    # where P is a permutation matrix, L is lower triangular with unit
-    # diagonal elements (lower trapezoidal if m > n), and U is upper
-    # triangular (upper trapezoidal if m < n).
-    #
-    # This is the right-looking Level 3 BLAS version of the algorithm.
-    @eval function getrf!(A::BlockDiagonalMatrix{$elty})
-        n, nb = blocksize(A), nblocks(A)
-        lda  = n
-        info = Vector{BlasInt}(nb)
-        ipiv = BlockVector(Array(BlasInt, n, nb))
-        for k = 1:nb
-            ccall(($(string(getrf)), liblapack), Void,
-                  (Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$elty},
-                   Ptr{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt}),
-                  &n, &n, pointer(A.data, _blockstart(A, k)), &lda, 
-                  pointer(ipiv.data, _blockstart(ipiv, k)), pointer(info, k))
-            info[k] < 0 && throw(ArgumentError("invalid argument #$(info[k])"))
-            info[k] > 0 && throw(SingularException("block $k is singular"))
-        end
-        A, ipiv, info
-    end
-
-    # DGETRS solves a system of linear equations
-    # A * X = B  or  A**T * X = B
-    # with a general N-by-N matrix A using the LU factorization 
-    # computed by DGETRF
-    @eval function getrs!(trans::Char, 
-                          A::BlockDiagonalMatrix{$elty}, 
-                          ipiv::BlockVector{BlasInt}, 
-                          info::Vector{BlasInt},
-                          B::BlockVector{$elty})
-        Base.LAPACK.chktrans(trans)
-        n = blocksize(A)
-        n != blocksize(B) && throw(DimensionMismatch("block sizes must match"))
-        for k = 1:nblocks(A)
-            ccall(($(string(getrs)), liblapack), Void,
-                  (Ptr{UInt8}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$elty}, Ptr{BlasInt},
-                   Ptr{BlasInt}, Ptr{$elty}, Ptr{BlasInt}, Ptr{BlasInt}),
-                  &trans, &n, &1, pointer(A.data, _blockstart(A, k)), 
-                  &n, pointer(ipiv.data, _blockstart(ipiv, k)), 
-                  pointer(B.data, _blockstart(B, k)), &n, pointer(info, k))
-            info[k] < 0 && throw(ArgumentError("invalid argument #$(info[k])"))
-        end
-        B
-    end
-end
-
-
 # ~~~ Linear algebra for bordered systems ~~~
 
-# Generic entry point for bordered systems
+A_ldiv_B!(M::BorderedMatrix, r::AbstractVector, alg::Symbol=:BED) = 
+    A_ldiv_B!(M, BorderedVector(slice(r[1:end-1]), r[end]), :BED)
+
 function A_ldiv_B!(M::BorderedMatrix, r::BorderedVector, alg::Symbol=:BED)
     # Solve the system
     #         
--- tests/runtests.jl
+++ tests/runtests.jl
@@ -1,94 +1,12 @@
 using Base.Test
 using BorderedArrays
-import Base.LinAlg: A_ldiv_B!, 
-                    At_ldiv_B!, 
-                    Ac_ldiv_B!    
-
-# test block vector
-let 
-    x = BlockVector([1 3 5;
-                     2 4 6])
-    @test eltype(x) == Int
-    for i = 1:length(x)
-        x[i] = i+1
-        @test x[i] == i+1
-        x[i] = i
-    end
-    @test full(x) == [1, 2, 3, 4, 5, 6]
-    @test size(x) == (6, )
-    @test_throws BoundsError size(x, 0)
-    @test size(x, 1) == 6
-    @test size(x, 2) == 1
-    @test length(x) == 6
-    @test nblocks(x) == 3
-    @test blocksize(x) == 2
-    @test BorderedArrays._blockstart(x, 1) == 1
-    @test BorderedArrays._blockstart(x, 2) == 3
-    @test BorderedArrays._blockstart(x, 3) == 5
-    @test_throws BoundsError BorderedArrays._blockstart(x, 0)
-    @test_throws BoundsError BorderedArrays._blockstart(x, 4)
-
-    # test dot product
-    x = BlockVector(2.0*[1 3 5;
-                         2 4 6])
-    y = BlockVector(0.5*[1 3 5;
-                         2 4 6])
-    @test dot(x, y) == 1*1 + 2*2 + 3*3 + 4*4 + 5*5 + 6*6
-end
-
-# test block matrix
-let 
-    A = BlockDiagonalMatrix([1 3 5;          
-                             2 3 4;         
-                             2 3 4;         
-                             2 4 6])
-    @test eltype(A) == Int
-    for i = 1:size(A, 1)
-        @test A[i, i] == i
-    end
-    @test full(A) == [1  2  0  0  0  0;
-                      2  2  0  0  0  0;
-                      0  0  3  3  0  0;
-                      0  0  3  4  0  0;
-                      0  0  0  0  5  4;
-                      0  0  0  0  4  6]                  
-    @test size(A) == (6, 6)
-    @test_throws BoundsError size(A, 0)
-    @test size(A, 1) == 6
-    @test size(A, 2) == 6
-    @test size(A, 3) == 1
-    @test nblocks(A) == 3
-    @test blocksize(A) == 2
-    @test BorderedArrays._blockstart(A, 1) == 1
-    @test BorderedArrays._blockstart(A, 2) == 5
-    @test BorderedArrays._blockstart(A, 3) == 9
-    @test_throws BoundsError BorderedArrays._blockstart(A, 0)
-    @test_throws BoundsError BorderedArrays._blockstart(A, 4)
-
-    # test block setindexing 
-    for k = 1:nblocks(A)
-        for i = 1:blocksize(A), j = 1:blocksize(A)
-            A[k, i, j] = k # these are local indices
-        end
-    end
-    @test full(A) == [1  1  0  0  0  0;
-                      1  1  0  0  0  0;
-                      0  0  2  2  0  0;
-                      0  0  2  2  0  0;
-                      0  0  0  0  3  3;
-                      0  0  0  0  3  3]
-    # test block getindexing                      
-    for k = 1:nblocks(A)
-        for i = 1:blocksize(A), j = 1:blocksize(A)
-            @test A[k, i, j] == k # these are local indices
-        end
-    end                      
-end
+import Base.LinAlg: A_ldiv_B!,
+                    At_ldiv_B!,
+                    Ac_ldiv_B!
 
 # test bordered block vector
 let
-    x = BorderedVector(BlockVector([1 3 5;
-                                    2 4 6]), 7)
+    x = BorderedVector([1, 2, 3, 4, 5, 6], 7)
     @test length(x) == 7
     @test size(x) == (7, )
     @test_throws BoundsError size(x, 0)
@@ -109,127 +27,68 @@ let
     @test_throws BoundsError x[8] = 1
 
     # more sizes
-    for n = 1:10, nb = 1:10
-        upp = BlockVector(rand(Float64, n, nb))
-        low = 0.0
+    for n = 5:100
+        upp = rand(Float64, n)
+        low = rand()
         x = BorderedVector(upp, low)
-        @test size(x) == (n*nb+1, )
-        @test size(x, 1) == n*nb+1
+        @test size(x) == (n+1, )
+        @test size(x, 1) == n+1
         @test size(x, 2) == 1
         @test_throws BoundsError size(x, 0)
         @test full(x) == x
         @test_throws BoundsError x[0]
-        @test_throws BoundsError x[n*nb+1 + 1]
+        @test_throws BoundsError x[n+1 + 1]
         @test eltype(x) == Float64
     end
 end
 
-# test bordered block matrix
-let 
-    A = BlockDiagonalMatrix([1 2 3;          
-                     2 3 4;         
-                     2 3 4;         
-                     1 2 3])
-    b = BlockVector([1 3 5;
-                     2 4 6])
-    c = BlockVector([6 4 2;
-                     5 3 1])
+# test bordered matrix
+let
+    A = [1 2 3 1;
+         2 3 4 1;
+         2 3 4 1;
+         1 2 3 1]
+    b = [1, 2, 3, 4]
+    c = [4, 3, 2, 1]
     d = 0
     M = BorderedMatrix(A, b, c, d)
-    @test size(M) == (7, 7)
-    @test size(M, 1) == size(M, 2) == 7
-    @test full(M) == [1  2  0  0  0  0  1;
-                      2  1  0  0  0  0  2;
-                      0  0  2  3  0  0  3;
-                      0  0  3  2  0  0  4;
-                      0  0  0  0  3  4  5;
-                      0  0  0  0  4  3  6;
-                      6  5  4  3  2  1  0] == M
+    @test size(M) == (5, 5)
+    @test size(M, 1) == size(M, 2) == 5
+    @test full(M) == [1  2  3  1  1;
+                      2  3  4  1  2;
+                      2  3  4  1  3;
+                      1  2  3  1  4;
+                      4  3  2  1  0] == M
     @test_throws BoundsError M[0, 0]
-    @test_throws BoundsError M[1, 8]
-    @test_throws BoundsError M[8, 1]
-    @test_throws BoundsError M[8, 8]
+    @test_throws BoundsError M[1, 6]
+    @test_throws BoundsError M[6, 1]
+    @test_throws BoundsError M[6, 6]
     @test eltype(M) == Int
-
-    # more sizes
-    for n = 1:10, nb = 1:10
-        A = BlockDiagonalMatrix(rand(Float64, n*n, nb))
-        b = BlockVector(rand(Float64, n, nb))
-        c = BlockVector(rand(Float64, n, nb))
-        d = 0.0
-        M = BorderedMatrix(A, b, c, d)
-        @test size(M) == (n*nb+1, n*nb+1)
-        @test size(M, 1) == size(M, 2) == n*nb+1
-        @test full(M) == M
-        @test_throws BoundsError M[0, 0]
-        @test_throws BoundsError M[1, n*nb+2]
-        @test_throws BoundsError M[n*nb+2, 1]
-        @test_throws BoundsError M[n*nb+2, n*nb+2]
-        @test eltype(M) == Float64
-    end
-end  
-       
-# test solution of block diagonal matrix 
-let 
-    # demo block matrix
-    srand(0)
-    for typ in (Float64, Complex128)
-        for (ch, fname) in zip(('N', 'T'), (A_ldiv_B!, At_ldiv_B!))   
-            for n = 1:10, nb = 1:10
-                A = BlockDiagonalMatrix(rand(typ, n*n, nb))
-                b = BlockVector(rand(typ, n, nb))
-            
-                # take copy for testing
-                Ad = full(A)
-                bd = full(b)
-
-                # solutions
-                xd, td, _, _, _ = @timed fname(lufact!(Ad), bd)
-                x, t, _, _, _   = @timed fname(lufact!(A), b)
-
-                # speed up
-                # speedup = td/t
-                # @printf "%s %3d - %3d - %5.2f\n" typ n nb speedup
-                
-                # check
-                @test xd ≈ x
-            end
-        end
-    end
 end
 
 # test solution of bordered matrix
-let 
+let
     srand(0)
-    for typ in [Float64, Complex128]
-        for n = 1:10, nb = 1:10
-            # demo matrices
-            A = BlockDiagonalMatrix(rand(typ, n*n, nb))
-            b = BlockVector(rand(typ, n, nb))
-            c = BlockVector(rand(typ, n, nb))
-            d = rand(typ)
-            M = BorderedMatrix(A, b, c, d)
-            Md = full(M)
+    for n = 10:100
+        # demo matrices
+        A = rand(n, n)
+        b = rand(n)
+        c = rand(n)
+        d = rand()
+        M = BorderedMatrix(A, b, c, d)
+        Md = full(M)
 
-            # demo array
-            x = BlockVector(rand(typ, n, nb))
-            y = rand(typ)
-            r = BorderedVector(x, y)
-            rd = full(r)
+        # demo array
+        x = rand(n)
+        y = rand()
+        r = BorderedVector(x, y)
+        rd = full(r)
 
-            # solutions
-            # r = A_ldiv_B!(M, r, :BED)
-            # rd = Md\rd
+        # solutions
+        r = A_ldiv_B!(M, r, :BED)
+        rd = Md\rd
 
-            rd, td, _, _, _ = @timed Md\rd
-            r, t, _, _, _   = @timed A_ldiv_B!(M, r, :BED)
-
-            # # speed up
-            # speedup = td/t
-            # @printf "%s %3d - %3d - %5.2f\n" typ n nb speedup
-
-            # check
-            @test r ≈ rd
-        end
+        # check
+        @test r ≈ rd
     end
 end
